f8663edf705b429b4c03242fb681c5ed36b5896d
ftmd909 <ftmd909@gmail.com>
2022-09-12 15:21:57 +0300

    Test

--- src/test/java/Drivers/FirefoxDrivers.java
+++ src/test/java/Drivers/FirefoxDrivers.java
@@ -2,8 +2,6 @@ package Drivers;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,8 +15,8 @@ public class FirefoxDrivers {
     @BeforeMethod
     public void setUp(){
 
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
         driver.get("https://www.a101.com.tr/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
